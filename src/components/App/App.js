f3e08883fd16899b0af7aa0fa0e14d13c095aea7
mariebawanan <bawananmarieflor@gmail.com>
2019-04-23 23:58:23 +0800

    Install react-router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { BrowserRoter } from 'react-router-dom';
+
 class App extends Component {
 	render() {
 		return <div>App</div>;
