710374b7cd3542716b4c09ad85d1aca9fdcef03c
mariebawanan <bawananmarieflor@gmail.com>
2019-04-25 09:33:39 +0800

    Added SidePanel and messages component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
+import { Grid } from 'semantic-ui-react';
 
-class App extends Component {
-	render() {
-		return <div>App</div>;
-	}
-}
+import SidePanel from '../SidePanel/SidePanel';
+import Messages from '../Messages/Messages';
+
+const App = () => {
+	return (
+		<Grid>
+			<SidePanel />
+			<Messages />
+		</Grid>
+	);
+};
 
 export default App;
