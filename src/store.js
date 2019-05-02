3d2e63b438dedcab9d8c812441efb4ba05d1380c
mariebawanan <bawananmarieflor@gmail.com>
2019-05-02 23:46:45 +0800

    Use redux dev tools through npm

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 
 const initialState = {};
@@ -8,12 +9,9 @@ const middleware = [thunk];
 
 // reducer, initial state, enhancers
 const store = createStore(
-	rootReducer,
-	initialState,
-	compose(
-		applyMiddleware(...middleware),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-	),
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware)),
 );
 
 export default store;
